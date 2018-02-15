397ebe67526e13526def14ffb516c35230ad26ec
patrickatcw <pj8392@yahoo.com>
2018-02-15 10:48:10 -0500

    initial while code

--- src/Practice.java
+++ src/Practice.java
@@ -2,6 +2,19 @@ public class Practice {
 
     public static void main(String[] args) {
 
+        int count = 1;                  //int value starts where the count begins
+        while (count < 11) {            //evaluating a boolean, while (true)
+            System.out.println("Count is: " + count);
+            count++;
+        }
+
     }
 
 }
+
+/*
+The while statement evaluates expression, which must return a boolean value.
+If the expression evaluates to true, the while statement executes the statement(s)
+in the while block. The while statement continues testing the expression and executing
+its block until the expression evaluates to false.
+*/
